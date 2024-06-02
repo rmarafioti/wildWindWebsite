42a633d272594660dbac7d5d2952b1b15e89bcab
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-06-01 22:30:18 -0500

    implemented hamburger menu / drop down

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,26 +1,36 @@
-import { NavLink } from "react-router-dom";
+import React, { useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    setMenuOpen(false);
+  }, [location.pathname]);
+
   return (
     <nav>
-      <h1>NAVBAR</h1>
-      <ul>
-        <li>
-          <NavLink to="/">HOME ICON</NavLink>
-        </li>
-        <li>
-          <NavLink to="/tattoos">TATTOOS</NavLink>
-        </li>
-        <li>
-          <NavLink to="/shop">SHOP</NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact">CONTACT</NavLink>
-        </li>
-        <li>
-          <NavLink to="/faqs">FAQs</NavLink>
-        </li>
-      </ul>
+      <h3>Home Icon</h3>
+      <h1>WILD WIND TATTOO</h1>
+      <div id="hamMenu" onClick={() => setMenuOpen(!menuOpen)}>
+        <div className={`menuButtonBurger ${menuOpen ? "open" : ""}`}></div>
+        <menu className={`menu ${menuOpen ? "active" : ""}`}>
+          <li className="navItem">
+            <NavLink to="/tattoos">TATTOOS</NavLink>
+          </li>
+          <li className="navItem">
+            <NavLink to="/shop">SHOP</NavLink>
+          </li>
+          <li className="navItem">
+            <NavLink to="/contact">CONTACT</NavLink>
+          </li>
+          <li className="navItem">
+            <NavLink to="/faqs">FAQs</NavLink>
+          </li>
+        </menu>
+        <div className={`overlay ${menuOpen ? "active" : ""}`}></div>
+      </div>
     </nav>
   );
 }
