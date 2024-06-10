bd0502c3f3e772cb03dce3a7c6f63f7241729b80
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-06-09 21:23:14 -0500

    created new faqs js file array of objects with an id, question and answer key

---
+++ src/faqs2.js
@@ -0,0 +1,56 @@
+export const faqs = [
+  {
+    id: 1,
+    question: "Does Wild Wind Tattoo accept walk-ins?",
+    answer:
+      "WWT strives to accommodate all clients and may accept walk-ins if openings are available. We recommend calling ahead on the day you plan to visit to check availability. Walk-ins are ideal for smaller tattoos that require minimal preparation. Please note that walk-ins are first-come, first-served, and there may be a wait if you don't have an appointment.",
+  },
+  {
+    id: 2,
+    question: "What is the shop minimum?",
+    answer:
+      "Our shop minimum is $100. The final price of any tattoo will be determined in person based on the design.",
+  },
+  {
+    id: 3,
+    question: "Is the shop cash only?",
+    answer:
+      "No, we accept all major credit cards and cash. Please note that we do not accept checks, Venmo, Square Cash, Chase Quick Pay, or similar payment methods.",
+  },
+  {
+    id: 4,
+    question: "Do you give price quotes via email or phone?",
+    answer:
+      "We do not provide valid price quotes via email or phone. We can offer a general price range, but this is only an estimate. The final price depends on the tattoo's size, detail, and placement, which we'll discuss during your in-person visit.",
+  },
+  {
+    id: 5,
+    question: "What is the wait time for getting an appointment?",
+    answer:
+      "Wait times vary as each artist manages their own schedule, but typically, we schedule appointments within a two to three-week timeframe.",
+  },
+  {
+    id: 6,
+    question: "How can I make an appointment?",
+    answer:
+      'Begin booking by filling out our <Link to="/contact">contact form</Link with basic information about your desired tattoo. We will respond within 1-3 business days. After an artist responds to your request, you can email them with reference photos and a design description. All tattoo appointments require a deposit. Once a date and time are agreed upon, you will be directed to complete our deposit form to secure your appointment.',
+  },
+  {
+    id: 7,
+    question: "Do I have to leave a deposit to make an appointment?",
+    answer:
+      "Yes, a $60 deposit is required for each appointment. Once the date and time are arranged, you will be directed to our deposit form. Your appointment is not confirmed until we receive your deposit. This deposit will be deducted from the cost of your tattoo when it is completed, or if your tattoo requires multiple sessions, it will be deducted during the last session.",
+  },
+  {
+    id: 8,
+    question: "Can I see my tattoo design before my appointment?",
+    answer:
+      "We do not send out or show tattoo designs via email before appointments, as we believe the best collaboration between client and artist occurs in person. Your design will be ready for review on the day of your appointment, and we will set aside enough time to make any revisions or changes you might want.",
+  },
+  {
+    id: 9,
+    question: "What is your cancellation policy?",
+    answer:
+      "If you cancel your appointment with at least 48 hours notice, we will honor your $60 deposit for a future appointment. Cancellations made with less than 48 hours notice will result in the forfeiture of your deposit. To reschedule, you may be required to place another deposit.",
+  },
+];
