56f0a2af965557deca4ac7c8a06d9af21f6bf3e0
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-06-18 22:26:40 -0500

    final commit tuesday

--- src/RequestAppt.jsx
+++ src/RequestAppt.jsx
@@ -6,17 +6,8 @@ import "./styling/contact.css";
 
 export default function RequestAppt() {
   return (
-    <main id="contact">
-      <h1 id="header">Request An Appointment</h1>
-      <section id="contactHeader">
-        <h3 className="headerTagline">
-          For general questions please review our{" "}
-          <Link id="reviewLink" to="/faqs">
-            FAQs
-          </Link>{" "}
-          first
-        </h3>
-      </section>
+    <main id="requestContact">
+      <h1 id="requestHeader">Request An Appointment</h1>
       <Form />
     </main>
   );
