2eeaed7b2ca2f50154dd3f570a59fb93215ef8a9
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-06-27 08:03:55 -0500

    created breakpoint to show contact button on landing page in mobile

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <main id="home">
       <h1 id="headerHome">HOME</h1>
+      <Link id="contactContainer" to="/contact">
+        <button id="contactHomeButton">Contact Us</button>
+      </Link>
     </main>
   );
 }
