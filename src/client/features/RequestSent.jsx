b10868a98057479c00ce013d275ff59ac336d52b
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-06-30 10:07:12 -0500

    adjusted layout on contact form request sent page

--- src/client/features/RequestSent.jsx
+++ src/client/features/RequestSent.jsx
@@ -5,14 +5,20 @@ import "./form.css";
 export default function RequestSent() {
   return (
     <main id="requestSent">
-      <h1 id="requestSentMessage">Thank you for your message!</h1>
-      <h3>Your request has been sent</h3>
-      <h3 id="requestMessage">
-        You can expect a response within 1-3 days. We are excited to work with
-        you soon!
-      </h3>
-      <h3 className="messageTag">Best,</h3>
-      <h3 className="messageTag">Wild Wind Tattoo</h3>
+      <div id="requestSentContainer">
+        <h1 id="requestSentMessage">Thank you for your message!</h1>
+        <h3 className="requestMessage" id="requestMessageTop">
+          Your request has been sent
+        </h3>
+        <h3 className="requestMessage">
+          You can expect a response within 1-3 days. We are excited to work with
+          you soon!
+        </h3>
+        <h3 className="messageTag">Best,</h3>
+        <h3 className="messageTag" id="messageTagBottom">
+          Wild Wind Tattoo
+        </h3>
+      </div>
       <Link id="returnLink" to="/">
         <h4 id="returnHome">Return Home</h4>
       </Link>
