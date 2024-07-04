d042667823a15a17ca268250381c8aaec397b3dd
Richard Marafioti <marafioti@MacBook-Air.cable.rcn.com>
2024-07-04 14:29:24 -0500

    added some css to homepage

--- src/client/features/Home.jsx
+++ src/client/features/Home.jsx
@@ -16,20 +16,19 @@ export default function Home() {
           content="This is the home page of wildwindtattoo.com."
         />
       </Helmet>
-      <h1 id="headerHome">HOME</h1>
       <Link id="contactContainer" to="/contact">
         <button id="contactHomeButton">Contact Us</button>
-        <p>
+        <p className="homeParagraph">
           Ready to get inked? Book your appointment today or visit us at 1452 N.
           Western Ave. Chicago IL 60622. Have questions? Contact us at
           773.230.0223 or wildwindtattoo@gmail.com.
         </p>
       </Link>
-      <h2>
-        Welcome to Wild Wind Tattoo – Expert Tattooing in Chicago's Vibrant
-        Wicker Park
+      <h1 id="headerHome">Welcome to Wild Wind Tattoo</h1>
+      <h2 className="headerTwo">
+        Expert Tattooing in Chicago's Vibrant Wicker Park
       </h2>
-      <p>
+      <p className="homeParagraph">
         Founded by Rich Marafioti in 2015, Wild Wind Tattoo has been a beacon of
         expert tattooing in Chicago. Located in the heart of Wicker Park, our
         studio offers a bright, welcoming, and relaxing space for clients to get
@@ -38,8 +37,8 @@ export default function Home() {
         environment where you will enjoy a positive experience that stays with
         you as long as your tattoo.
       </p>
-      <h2>Community Engagement</h2>
-      <p>
+      <h2 className="headerTwo">Community Engagement</h2>
+      <p className="homeParagraph">
         Wild Wind Tattoo proudly serves and works with our strong community.
         Over the years, we have held fundraisers supporting families of Pulse
         Nightclub shooting victims, Project Fierce, and the Chicago Abortion
@@ -47,19 +46,19 @@ export default function Home() {
         Hire Trans Now initiative. Our commitment to community and inclusivity
         is at the core of our mission.
       </p>
-      <h2>Expert Tattoo Services</h2>
-      <ul>
-        <li>
-          <h3>Variety and Customization</h3>
-          <p>
+      <h2 className="headerTwo">Expert Tattoo Services</h2>
+      <ul id="tattooServices">
+        <li className="listItemContainer">
+          <h3 className="headerThree">Variety and Customization:</h3>
+          <p className="homeParagraph">
             We offer various tattoo styles, from small walk-ins to larger custom
             pieces. Whether you have a design in mind or need inspiration, our
             talented artists are here to bring your vision to life. Just ask!
           </p>
         </li>
-        <li>
-          <h3>Comfort and Professionalism</h3>
-          <p>
+        <li className="listItemContainer">
+          <h3 className="headerThree">Comfort and Professionalism:</h3>
+          <p className="homeParagraph">
             At Wild Wind Tattoo, you can expect to be treated with respect and
             care. We communicate all aspects of your tattoo experience, from
             consultation to aftercare. Enjoy a clean, creative, and artistic
@@ -67,31 +66,33 @@ export default function Home() {
           </p>
         </li>
       </ul>
-      <h2>Why Choose Wild Wind Tattoo?</h2>
-      <ul>
-        <li>
-          <h3>Experienced Artists:</h3>
-          <p>
+      <h2 className="headerTwo">Why Choose Wild Wind Tattoo?</h2>
+
+      <ul id="whyChoose">
+        <li className="whyChooseItem">
+          <h3 className="headerThree">Experienced Artists:</h3>
+          <p className="homeParagraph">
             Our team, led by founder Rich Marafioti, brings years of expertise
             and passion to every tattoo.
           </p>
         </li>
         <li>
-          <h3>Community Focus:</h3>
-          <p>
+          <h3 className="headerThree">Community Focus:</h3>
+          <p className="homeParagraph">
             We actively engage with and support our local community through
             various initiatives and partnerships.
           </p>
         </li>
         <li>
-          <h3>Inclusive Space:</h3>
-          <p>
+          <h3 className="headerThree">Inclusive Space:</h3>
+          <p className="homeParagraph">
             We are committed to providing a welcoming environment for all
             clients, ensuring everyone feels comfortable and respected.
           </p>
         </li>
       </ul>
-      <h2>Client Testimonials</h2>
+
+      <h2 className="headerTwo">Client Testimonials</h2>
     </main>
   );
 }
